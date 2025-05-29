d740d5b45d9f747b3b8ae5b7fa4c900abe561b5c
LiamTec <liam.gonzales@tecsup.edu.pe>
2025-05-29 15:11:40 -0500

    todo

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
@@ -1,37 +1,53 @@
 package com.tecsup.petclinic.services;
 
-import lombok.extern.slf4j.Slf4j;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.tecsup.petclinic.entities.Owners;
+import com.tecsup.petclinic.repositories.OwnersRepository;
+import com.tecsup.petclinic.util.OwnersObjectCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
 @Slf4j
 public class OwnerServiceTest {
 
-	//@Autowired
-   //	private OwnerService ownerService;
+	@Autowired
+	private OwnersService ownerService;
 
-	/**
-	 * 
-	 */
-	/*
+	@Autowired
+	private OwnersRepository ownersRepository;
+
 	@Test
-	public void testFindOwnerById() {
+	public void testCreateOwner() {
 
-		long ID = 1;
-		String NAME = "Jaime";
-		Owner owner = null;
-		
-		try {
-			
-			owner = ownerService.findById(ID);
-			
-		} catch (OwnertNotFoundException e) {
-			fail(e.getMessage());
-		}
-		log.info("" + owner);
+		// Datos esperados
+		String FIRST_NAME = "Josue";
+		String LAST_NAME = "Mario";
+		String ADDRESS = "Bros";
+		String CITY = "Mushroom Kingdom";
+		String TELEPHONE = "123456789";
 
-		assertEquals(NAME, owner.getName());
+		// Crear objeto Owners sin ID (nuevo)
+		Owners owner = OwnersObjectCreator.newOwner();
+
+		// Crear el owner en la base (servicio)
+		Owners ownerCreated = this.ownerService.create(owner);
+
+		log.info("OWNER CREATED: " + ownerCreated.toString());
+
+		// Validaciones
+		assertNotNull(ownerCreated.getId());  // Se generó ID al guardar
+		assertEquals(FIRST_NAME, ownerCreated.getFirst_name());
+		assertEquals(LAST_NAME, ownerCreated.getLast_name());
+		assertEquals(ADDRESS, ownerCreated.getAddress());
+		assertEquals(CITY, ownerCreated.getCity());
+		assertEquals(TELEPHONE, ownerCreated.getTelephone());
 
 	}
-	*/
 }
